12f7f61ac453466086b69e66631cf4b2fdd3a6e9
Thiago3011 <thiago.silva1001@outlook.com>
2024-11-28 21:02:08 -0300

    finalizando as rotas

--- app.py
+++ app.py
@@ -31,8 +31,7 @@ def add_product():
         db.session.commit()
     
         return jsonify({"message": "Product added sucessfully"}), 200
-    else:
-        return jsonify({"message": "Invalid product data"}), 400
+    return jsonify({"message": "Invalid product data"}), 400
 
 @app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
 def delete_product(product_id):
@@ -41,10 +40,62 @@ def delete_product(product_id):
         db.session.delete(product)
         db.session.commit()
         
-        return jsonify({"message": "Product deleted sucessfully"}), 200
-    else:
+        return jsonify({"message": "Product deleted sucessfully"})
+    return jsonify({"message": "Product not found"}), 404
+    
+@app.route('/api/products/<int:product_id>', methods=["GET"])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    
+    if product:
+        return jsonify({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "description": product.description
+        })
+        
+    return jsonify({"message": "Product not found"}), 404
+    
+@app.route('/api/products/update/<int:product_id>', methods=["PUT"])
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    
+    if not product:
         return jsonify({"message": "Product not found"}), 404
     
+    data = request.json
+    
+    if "name" in data:
+        product.name = data["name"] 
+    
+    if "price" in data:
+        product.price = data["price"]
+        
+    if "description" in data:
+        product.description = data["description"]
+        
+    db.session.commit()
+        
+    return jsonify({"message": "Product updated sucessfully"})
+
+@app.route('/api/products', methods=["GET"])
+def get_products():
+    products = Product.query.all()
+    
+    product_list = []
+    
+    for product in products:
+        product_data = {
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+        }
+        
+        product_list.append(product_data)
+
+    return jsonify(product_list)
+
 # Execução do servidor Flask
 @app.route('/')
 def hello_world():
